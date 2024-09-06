89767b7d1d5840ad2be3250ecf73a01ed656c3e3
mmasap <miura.worker@gmail.com>
2024-09-06 16:30:55 +0900

    sort select prefecture

--- src/hooks/useSelectPrefecture.ts
+++ src/hooks/useSelectPrefecture.ts
@@ -2,16 +2,14 @@ import { PrefectureResult } from '@/lib/api'
 import { useState } from 'react'
 
 export const useSelectPrefecture = () => {
-  const [selectPrefectures, setSelectPrefectures] = useState<
-    PrefectureResult[]
-  >([])
+  const [selectPrefectures, setSelectPrefectures] = useState<PrefectureResult[]>([])
 
   function handleSelectPrefecture(prefecture: PrefectureResult) {
     setSelectPrefectures((prev) => {
       if (prev.some((p) => p.prefCode === prefecture.prefCode)) {
         return prev.filter((p) => p.prefCode !== prefecture.prefCode)
       } else {
-        return [...prev, prefecture]
+        return [...prev, prefecture].sort((a, b) => a.prefCode - b.prefCode)
       }
     })
   }
