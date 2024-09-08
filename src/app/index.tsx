395ce9cf087bbf49546a6f7d43ed9bdd163886db
mmasap <miura.worker@gmail.com>
2024-09-09 08:35:23 +0900

    move feature folder

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import { useSelectPrefecture } from '@/hooks/useSelectPrefecture'
 import { AppProvider } from './provider'
-import { SelectPrefecture, Chart } from '@/components/features/population'
+import { SelectPrefecture, Chart } from '@/features/population'
 
 export const App = () => {
   const { selectPrefectures, handleSelectPrefecture } = useSelectPrefecture()
