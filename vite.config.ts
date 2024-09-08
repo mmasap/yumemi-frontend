969af8d611aec3376a416c80e8eb2cd70783c5b5
mmasap <miura.worker@gmail.com>
2024-09-08 11:11:17 +0900

    fix build base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/yumemi-frontend' : '/',
   plugins: [react()],
   resolve: {
     alias: {
