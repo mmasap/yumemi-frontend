f86a71bc8d30ed2e1c3b0298797916aba16ca8fe
mmasap <miura.worker@gmail.com>
2024-09-13 11:06:46 +0900

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,26 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 import tseslint from 'typescript-eslint'
 import playwright from 'eslint-plugin-playwright'
+import prettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'public'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+  },
+  {
+    files: ['src/**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+    extends: [react.configs.flat.recommended, react.configs.flat['jsx-runtime']],
+    settings: {
+      react: { version: 'detect' },
     },
     plugins: {
       'react-hooks': reactHooks,
@@ -27,4 +36,5 @@ export default tseslint.config(
     ...playwright.configs['flat/recommended'],
     files: ['tests/**'],
   },
+  prettier,
 )
